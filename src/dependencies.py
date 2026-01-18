118e6f89c341b61414284289e126761ad8ec95d2
Dmitry Zhigalo <zhdmts@gmail.com>
2026-01-18 21:32:45 +0100

    Minor fixes in imports

--- src/dependencies.py
+++ src/dependencies.py
@@ -3,7 +3,6 @@ from typing import Annotated, cast
 
 from fastapi import Depends, Request
 from fastapi.security import OAuth2PasswordRequestForm
-# from src.services.database.postgresql import Base
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config import Settings
@@ -26,7 +25,6 @@ async def get_async_db_session(request: Request) -> AsyncGenerator[AsyncSession]
 ##############################################################################################
 
 SettingsDep = Annotated[Settings, Depends(get_settings)]
-# AsyncDatabaseDep = Annotated[Base, Depends(get_async_db_session)]
 AsyncDatabaseDep = Annotated[AsyncSession, Depends(get_async_db_session)]
 OAuth2PasswordRequestFormDep = Annotated[OAuth2PasswordRequestForm, Depends()]
 
